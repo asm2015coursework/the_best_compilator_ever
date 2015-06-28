fe5cfef3af2b9a1a1d3c041625f5a754fcc715bc
nik <smnikita@list.ru>
2015-06-29 01:33:34 +0300

    empty for fix

--- tokens/ForToken.cpp
+++ tokens/ForToken.cpp
@@ -8,8 +8,16 @@ ForToken::ForToken(Token* expr1, Token* expr2, Token* expr3, Token* cmd) {
 }
 
 string ForToken::toString() {
-    string ans = "for (" + expr1->toString() + ";" + expr2->toString() + ";";
-    ans += expr3->toString() + ") " + cmd->toString();
+    //string ans = "for (" + expr1->toString() + ";" + expr2->toString() + ";";
+    //ans += expr3->toString() + ") " + cmd->toString();
+    string ans = "for (";
+    if (expr1 != nullptr) ans += expr1->toString();
+    ans += ";";
+    if (expr2 != nullptr) ans += expr2->toString();
+    ans += ";";
+    if (expr3 != nullptr) ans += expr3->toString();
+    ans += ") ";
+    if (cmd != nullptr) ans += cmd->toString();
     return ans;
 }
 
