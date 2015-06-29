39a03df1d8b20111003e70677d25fdeb996b7081
lightning95 <aydar.gizatullin@gmail.com>
2015-06-29 10:39:41 +0300

    tests

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #include <cstring>
 #include <iostream>
 #include <vector>
-
+#include <unistd.h>
+#include <wait.h>
 
 #include "tokens/Token.h"
 #include "Preprocessor.h"
@@ -15,44 +16,9 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
- /*
-    if (!(argc == 2 || (argc == 4 && strcmp(argv[2], "-o") == 0))) {
-        printf("Usage: ./the_best_compilator_ever <file to compile> -o <name of asm file>\n");
-        return 1;
-    }
-    std::string output_file = "a.out";
-    if (argc == 4) {
-        output_file = argv[3];
-    }
-    std::string code = Preprocessor::preprocess(argv[0]);
-    std::vector<Token*> parsed_code = (new Parser)->parse(code); //don't do like that!!!
-    std::string asm_code = CodeGenerator::generate(parsed_code);
-    printf("Compiled to %s \n", output_file.data());
-    freopen(output_file.data(), "w", stdout);
-    std::cout << asm_code;
-*/
-    Parser parser;
-    Preprocessor preproc;
 
-    preproc.setIncludeDirectory("");
-    string TEST = "test02";
-    preproc.preprocess("../the_best_compilator_ever/tests/" + TEST +".cmm","", "../the_best_compilator_ever/cmmlibs/");
-    vector<Token*> tokens = parser.parse(preproc.preprocess("../the_best_compilator_ever/tests/" + TEST +".cmm"));
-    string output = CodeGenerator::generate(tokens);
-    //for (size_t i = 0; i < tokens.size(); i++) {
-    //    output += tokens[i]->toString() + ";\n";
-    //}
-    //output += '\n';
-
-    //std::cout << output;
-    if (output != "") {
-        fileFromString(TEST + ".asm", output);
-        println(exec("yasm -felf64 -dgwarf2 "+ TEST + ".asm -o  "+ TEST + ".o"));
-        println(exec("gcc "+ TEST + ".o -o " + TEST));
-    }
-
-    //Tester t;
-    //t.run_all();
+    Tester tester;
+    tester.run_one(argv[1]);
 
     return 0;
 }
